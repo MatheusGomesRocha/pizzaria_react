8415063cb4c0b82cd8822c0fc0dc4d816bd57125
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-26 09:50:43 -0300

    Criando variáveis globais + finalizando design do Component Introduction + Aplicando alguns estilos apenas para telas maiories que 2000px

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './globals.scss';
 
 import Introduction from './components/Introduction';
 
--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -7,6 +7,18 @@ import styles from './styles.module.scss';
 export default function Introduction () {
     return(
         <div className={styles.container}>
+            <div className={styles.content}>
+                <h1>nunc pulvinar et</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Consectetur libero id faucibus nisl tincidunt. Non odio euismod lacinia at quis risus sed vulputate. Tincidunt augue interdum velit 
+                    uismod in pellentesque. Eu volutpat odio facilisis mauris sit amet.
+                </p>
+                <div className={styles.button}>
+                    <span>Read More</span>
+                </div>
+            </div>
+
             <img src={PizzaImg} />
         </div>
     )
