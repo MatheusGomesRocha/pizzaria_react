a66d3a481b92194d48622b0f2e80480869c4a165
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-26 09:08:35 -0300

    Adicionando imagens de Pizza e Wood Background para usar no Component Introduction

--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import PizzaImg from '../../assets/images/pizza.png';
+
 import styles from './styles.module.scss';
 
 export default function Introduction () {
     return(
         <div className={styles.container}>
-
+            <img src={PizzaImg} />
         </div>
     )
 }
