639ccd34014b7635ef881f00fee0884c01e9dc26
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-27 14:47:28 -0300

    Mudando design do Component Introduction

--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -8,18 +8,14 @@ export default function Introduction () {
     return(
         <div className={styles.container}>
             <div className={styles.content}>
-                <h1>nunc pulvinar et</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Consectetur libero id faucibus nisl tincidunt. Non odio euismod lacinia at quis risus sed vulputate. Tincidunt augue interdum velit 
-                    uismod in pellentesque. Eu volutpat odio facilisis mauris sit amet.
-                </p>
+                <h2>Authentic Italian Pizzas</h2>
+
+                <h1>The Best Pizza in Town</h1>
+
                 <div className={styles.button}>
-                    <span>Read More</span>
+                    <span>Order online now</span>
                 </div>
             </div>
-
-            <img src={PizzaImg} />
         </div>
     )
 }
