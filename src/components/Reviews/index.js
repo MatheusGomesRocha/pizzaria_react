acc5b94d369f49789cf9695b684435c585c894d9
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-28 15:14:36 -0300

    Adicionando imagem maior para colocar no Background do Component Reviews + Finalizando o design deste Component (Ainda não inserido o Carrousel para passar as Reviews)

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -5,7 +5,20 @@ import styles from './reviews.module.scss';
 export default function Reviews () {
     return(
         <div className={styles.container}>
-            
+            <section>
+                <span className={styles.title}>Some good reviews</span>
+                <h1>What People Say</h1>
+
+                <span className={styles.aspas}>"</span>
+
+                <p>
+                    ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+
+                <span className={styles.name}>Clark Kent</span>
+            </section>
         </div>
     )
 }
