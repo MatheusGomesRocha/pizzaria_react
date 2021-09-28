a9c0ed3bf27f3245eaa2c9970f7e92794917b1f7
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-28 08:13:52 -0300

    Criando Component Features + Adicionando imagens de Lasanha e Ravioli para usar neste Component

---
+++ src/components/Features/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from './features.module.scss';
+
+export default function Features () {
+    return(
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
