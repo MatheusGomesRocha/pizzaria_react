746b4a1517d92d3fec44870d530b901fcfd7f582
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-28 14:47:42 -0300

    Finalizando design do Component Features

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Introduction from './components/Introduction';
 import Welcome from './components/Welcome';
 import Menu from './components/Menu';
+import Features from './components/Features';
 import About from './components/About';
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
       <Welcome />
 
       <Menu />
+
+      <Features />
 
       <About />
 
