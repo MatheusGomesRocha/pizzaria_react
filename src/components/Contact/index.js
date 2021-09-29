f773cb65ee85744439715204200dbd51ec7cb642
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-29 12:34:25 -0300

    Criando Component Contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PizzaImg1 from '../../assets/images/pizza-background.jpg';
+
+import styles from './contact.module.scss';
+
+export default function Contact () {
+    return(
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
