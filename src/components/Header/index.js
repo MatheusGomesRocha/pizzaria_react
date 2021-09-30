79dde18a0e6ab1b8bd83429b2435d584a119600b
Matheus Gomes <matheusgomes192@hotmail.com>
2021-09-30 16:54:10 -0300

    Adicionando Cart no Component Header e estilizando como os itens irão ficar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import PizzaLogo from '../../assets/images/pizza-logo.png';
+import Cart from '../../assets/images/shopping-cart.png';
+import PizzaDemo2 from '../../assets/images/pizza-demo-2.png';
 
 import styles from './header.module.scss';
 
 export default function Header () {
+    const [cartOpen, setCartOpen] = useState(false);
+    const [quantity, setQuantity] = useState(1);
+    const [price, setPrice] = useState(35.50);
+
+    function decrease () {
+        if(quantity > 1) {
+            setQuantity(quantity - 1);
+        }  else {
+            setQuantity(quantity);
+        }
+    }
+
+    function increase () {
+        if(quantity >= 1) {
+            setQuantity(quantity + 1);
+        }  else {
+            setQuantity(quantity);
+        }
+    }
+
     return(
         <div className={styles.header}>
             <nav>
@@ -25,8 +47,32 @@ export default function Header () {
                             <span>Book a Table</span>
                         </div>
                     </li>
+
+                    <li onClick={() => setCartOpen(!cartOpen)} className={styles.cart}>
+                        <img src={Cart} />
+                    </li>
                 </ul>
             </nav>
+
+            <section className={cartOpen ? styles.cartAreaOpen : styles.cartAreaClose}>
+                <div className={styles.cartItem}>
+                    <img src={PizzaDemo2} />
+
+                    <div className={styles.content}>
+                        <div className={styles.contentHeader}>
+                            <h2>Pepperonni</h2>
+                            <span>R$ {parseFloat(quantity * price)}</span>
+                        </div>
+
+                        <div className={styles.contentFooter}>
+                            <span onClick={decrease}>-</span>
+                            <span className={styles.quantity}>{quantity}</span>
+                            <span onClick={increase}>+</span>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
         </div>
     )
 }
