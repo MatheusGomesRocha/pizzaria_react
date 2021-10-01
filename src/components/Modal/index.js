ad525dbde8545f1e5636574413da8a8cd5f3a9ac
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-01 08:26:01 -0300

    Criando um Dividir para o Modal para mostrar apenas um Form de cada vez e fazendo sua animation de movimentação da esquerda para a direita e vice-versa

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from './modal.module.scss';
 
 export default function Modal () {
+    const [whatForm, setWhatForm] = useState('login');
+
     return(
         <div className={styles.container}>
-            
+            <div className={styles.modal}>
+                <section onClick={whatForm === 'login' ? () => setWhatForm('sign_up') : () => setWhatForm('login')} className={whatForm === 'login' ? styles.dividerShowLogin : styles.dividerShowSignUp} />
+            </div>
         </div>
     )
 }
