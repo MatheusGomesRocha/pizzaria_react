86881fc0bca6e61cf0768865ebe6ea6529901035
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-04 13:35:04 -0300

    Caso o usuário não esteja logado, ao tentar adicionar um produto ao Carrinho, abrirá o Modal para que faça o Login

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import PizzaDemo2 from '../../assets/images/pizza-demo-2.png';
 import PizzaDemo3 from '../../assets/images/pizza-demo-3.png';
 
+import { ModalContext } from '../../contexts/ModalContext';
+
 import styles from './menu.module.scss';
 
+let array = [
+    {id: 1, img: PizzaDemo3, name: 'Pepperoni', description: 'Tomato, salami, cheese, pepper, chilli, sauce, mozzarella cheese and onion', price: '18.50', new: true},
+    {id: 2, img: PizzaDemo2, name: 'Pepperoni', description: 'Tomato, salami, cheese, pepper, chilli, sauce, mozzarella cheese and onion', price: '18.50'},
+    {id: 3, img: PizzaDemo3, name: 'Pepperoni', description: 'Tomato, salami, cheese, pepper, chilli, sauce, mozzarella cheese and onion', price: '18.50'},
+];
+
 export default function Menu () {
+    const { email, openModal } = useContext(ModalContext);
+
     return(
         <div className={styles.container}>
             <section className={styles.header}>
@@ -14,39 +24,26 @@ export default function Menu () {
             </section>
 
             <section className={styles.menuArea}>
-                <div className={styles.item}>
-                    <div className={styles.newIndicator}>
-                        <span>New</span>
+                {array.map((item, k) => (
+                    <div key={k} className={styles.item}>
+                        {item.new ? 
+                            <div className={styles.newIndicator}>
+                                <span>New</span>
+                            </div>
+                        : undefined
+                        }
+                        
+                        <img src={item.img} />
+                        <span className={styles.name}>{item.name}</span>
+                        <p>{item.description}</p>
+                        <h2 className={styles.price}>R$ {item.price}</h2>
+                        <div onClick={email ? undefined : openModal} className={styles.button}>
+                            <span>Add to cart</span>
+                        </div>
                     </div>
-                    
-                    <img src={PizzaDemo3} />
-                    <span className={styles.name}>Pepperoni</span>
-                    <p>Tomato, salami, cheese, pepper, chilli, sauce, mozzarella cheese and onion</p>
-                    <h2 className={styles.price}>R$ 18.50</h2>
-                    <div className={styles.button}>
-                        <span>Order now</span>
-                    </div>
-                </div>
+                ))}
 
-                <div className={styles.item}>
-                    <img src={PizzaDemo2} />
-                    <span className={styles.name}>Pepperoni</span>
-                    <p>Tomato, salami, cheese, pepper, chilli, sauce, mozzarella cheese and onion</p>
-                    <h2 className={styles.price}>R$ 18.50</h2>
-                    <div className={styles.button}>
-                        <span>Order now</span>
-                    </div>
-                </div>
-
-                <div className={styles.item}>
-                    <img src={PizzaDemo3} />
-                    <span className={styles.name}>Pepperoni</span>
-                    <p>Tomato, salami, cheese, pepper, chilli, sauce, mozzarella cheese and onion</p>
-                    <h2 className={styles.price}>R$ 18.50</h2>
-                    <div className={styles.button}>
-                        <span>Order now</span>
-                    </div>
-                </div>
+                
             </section>
         </div>
     )
